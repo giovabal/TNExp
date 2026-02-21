4911f21d53860cb0f50e3a625628d19dea2f8f21
Giovanni Baldini <giovabal@gmail.com>
2026-02-21 23:54:10 +0100

    managing event loop in search channels

--- webapp/management/commands/search_channels.py
+++ webapp/management/commands/search_channels.py
@@ -1,5 +1,8 @@
+import asyncio
+
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.db.models import F
 from django.utils import timezone
 
 from webapp.crawler import TelegramCrawler
@@ -12,12 +15,19 @@ class Command(BaseCommand):
     args = ""
     help = "crawling Telegram groups"
 
+    def _ensure_event_loop(self):
+        try:
+            asyncio.get_running_loop()
+        except RuntimeError:
+            asyncio.set_event_loop(asyncio.new_event_loop())
+
     def handle(self, *args, **options):
+        self._ensure_event_loop()
         with TelegramClient("anon", settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH).start(
             phone=settings.TELEGRAM_PHONE_NUMBER
         ) as client:
             crawler = TelegramCrawler(client)
-            for term in SearchTerm.objects.all().order_by("last_check")[:15]:
+            for term in SearchTerm.objects.all().order_by(F("last_check").asc(nulls_first=True))[:15]:
                 crawler.search_channel(term.word)
                 term.last_check = timezone.now()
                 term.save(update_fields=["last_check"])
